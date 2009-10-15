781447471c066af3990e020b82393522a6feca5f
JamesD_cp <SND\JamesD_cp>
2009-10-15 10:36:47 +0000

    Added an initial automated build Tweaked solution structure

--- main/src/SSRSMSBuildTasks/ssrsmsbuildtasks/Properties/AssemblyInfo.cs
+++ main/src/SSRSMSBuildTasks/ssrsmsbuildtasks/Properties/AssemblyInfo.cs
@@ -17,12 +17,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ssrsmsbuildtasks")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ssrsmsbuildtasks")]
-[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyTitle("SSRSMSBuildTasks")]
+[assembly: AssemblyDescription("SQL Server Reporting Services MSBuild Tasks Library")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,14 +29,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("390e377b-82cc-48e5-a5d0-b2f0b9c0b153")]
-
-// Version information for an assembly consists of the following four values:
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ main/src/common/AssemblyVersion.cs
@@ -0,0 +1,2 @@
+[assembly: System.Reflection.AssemblyVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.0.0.0")]
