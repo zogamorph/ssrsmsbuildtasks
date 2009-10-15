c87db893a5e6d3459d119863e07d6cd9cea09c18
JamesD_cp <SND\JamesD_cp>
2009-10-15 10:36:47 +0000

    Added an initial automated build Tweaked solution structure

---
+++ main/src/common/CommonAssemblyInfo.cs
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------------------------- 
+// <copyright file="CommonAssemblyInfo.cs" company="StyleCop for ReSharper Development Team">
+// Copyright (c) StyleCop for ReSharper Development Team.  All rights reserved.
+// </copyright>
+// <summary>Sets whether the assembly was is in debug or release mode</summary>
+//-------------------------------------------------------------------------------------------------
+
+#if DEBUG 
+
+[assembly: System.Reflection.AssemblyConfiguration("Debug")] 
+
+#else 
+
+[assembly: System.Reflection.AssemblyConfiguration("Release")] 
+
+#endif
+
+[assembly: System.Reflection.AssemblyCompany("SSRSMSBuildTasks Development Team")]
+[assembly: System.Reflection.AssemblyProduct("SSRSMSBuildTasks")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © 2009")]
