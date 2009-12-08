f5a1217b69abee0460fc998f3e9a71a875c6af00
StevenJWright_cp <SND\StevenJWright_cp>
2009-12-08 16:10:04 +0000

    Update the Example project to include all the new changes made to the dll

---
+++ main/example/SSRSMSBuildTeaskDemo/NativeUploadReports.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+SET ReportingServer=%1
+
+IF "%ReportingServer%"=="?" GOTO :Error
+IF "%ReportingServer%"=="" GOTO :Error
+
+@%WinDir%\microsoft.net\Framework\v2.0.50727\MSBuild SSRSMSBuildTeaskDemo_Native.msbuild /t:FullDepoly /p:ReportingServerURL=%ReportingServer%
+GOTO :End 
+
+:Error
+@Echo Usage:
+@Echo "NativeUploadReports http://<ReportServer>/<ReportServerVirtualDirecory>"
+GOTO :End
+
+:End
+pause
+
