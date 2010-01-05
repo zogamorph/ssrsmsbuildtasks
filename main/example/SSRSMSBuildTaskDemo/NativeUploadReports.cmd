ba8c194b05741d1d1e0cdba5f4bc915271fa44d2
StevenJWright_cp <SND\StevenJWright_cp>
2010-01-05 10:05:55 +0000

    Update the Example project with all the latest changes.

--- main/example/SSRSMSBuildTaskDemo/NativeUploadReports.cmd
+++ main/example/SSRSMSBuildTaskDemo/NativeUploadReports.cmd
@@ -5,7 +5,7 @@ SET ReportingServer=%1
 IF "%ReportingServer%"=="?" GOTO :Error
 IF "%ReportingServer%"=="" GOTO :Error
 
-@%WinDir%\microsoft.net\Framework\v2.0.50727\MSBuild SSRSMSBuildTeaskDemo_Native.msbuild /t:FullDepoly /p:ReportingServerURL=%ReportingServer%
+@%WinDir%\microsoft.net\Framework\v2.0.50727\MSBuild SSRSMSBuildTaskDemo_Native.msbuild /t:FullDepoly /p:ReportingServerURL=%ReportingServer%
 GOTO :End 
 
 :Error
