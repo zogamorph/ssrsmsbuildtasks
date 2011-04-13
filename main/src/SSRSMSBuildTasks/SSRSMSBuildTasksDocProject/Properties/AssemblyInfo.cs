668e817760f7bffac5639dd4688674d3671dc180
Steve Wright <zogamorph@gmail.com>
2011-04-13 21:44:55 +0000

    Updating Source Control

--- main/src/SSRSMSBuildTasks/SSRSMSBuildTasksDocProject/Properties/AssemblyInfo.cs
+++ main/src/SSRSMSBuildTasks/SSRSMSBuildTasksDocProject/Properties/AssemblyInfo.cs
@@ -8,6 +8,10 @@ using System;
 // associated with an assembly.
 [assembly: AssemblyTitle("SSRSMSBuildTasksDocProject")]
 [assembly: AssemblyDescription("DocProject help compiler for SSRSMSBuildTasksDocProject.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Conchango")]
+[assembly: AssemblyProduct("SSRSMSBuildTasksDocProject")]
+[assembly: AssemblyCopyright("Copyright © 2011 Conchango")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -18,5 +22,14 @@ using System;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("202fcfbf-357d-41fd-819a-be4e94b264e1")]
+[assembly: Guid("42b8b06b-3cf0-48aa-8c49-91b8b4c10f9b")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
