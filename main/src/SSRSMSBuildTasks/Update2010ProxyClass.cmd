89ba1462a0e903a6bd968678b9984aa62b84e5fe
StevenJWright_cp <SND\StevenJWright_cp>
2011-04-13 21:44:55 +0000

    Updating Source Control

--- main/src/SSRSMSBuildTasks/Update2010ProxyClass.cmd
+++ main/src/SSRSMSBuildTasks/Update2010ProxyClass.cmd
@@ -4,7 +4,7 @@ IF "%1"=="?" GOTO :Error
 IF "%1"=="" GOTO :Error
 
 SET ReportingServer=%1/ReportService2010.asmx
-@Wsdl /language:c#  /protocol:SOAP /namespace:ssrsmsbuildtasks.DeploymentManger.Proxy.Native /out:.\ssrsmsbuildtasks\DeploymentManger\Proxy\NativeR2\MSReportingService2010.cs %1
+@Wsdl /language:c#  /protocol:SOAP /namespace:ssrsmsbuildtasks.DeploymentManger.Proxy.R2 /out:.\ssrsmsbuildtasks\DeploymentManger\Proxy\R2\MSReportingService2010.cs %1
 
 GOTO :End 
 
