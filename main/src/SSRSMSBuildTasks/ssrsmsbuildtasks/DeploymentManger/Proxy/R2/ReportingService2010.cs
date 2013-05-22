1185b03cc4d1b4d9d89fd8eae784981337ad2303
dsilen_cp <SND\dsilen_cp>
2013-05-22 06:58:01 +0000

    Added support for Forms Authentication in R2, Native Mode.

--- main/src/SSRSMSBuildTasks/ssrsmsbuildtasks/DeploymentManger/Proxy/R2/ReportingService2010.cs
+++ main/src/SSRSMSBuildTasks/ssrsmsbuildtasks/DeploymentManger/Proxy/R2/ReportingService2010.cs
@@ -7,6 +7,9 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.Net;
+
 namespace ssrsmsbuildtasks.DeploymentManger.Proxy.R2
 {
     /// <summary>
@@ -25,6 +28,12 @@ namespace ssrsmsbuildtasks.DeploymentManger.Proxy.R2
         public ReportingService2010(string reportingService2010Url)
         {
             this.Url = reportingService2010Url;
+
+            var credentials = GetCredentialsInUrl(reportingService2010Url);
+            if (credentials != null)
+            {
+                PerformFormsAuthentication(credentials);
+            }
         }
 
         #endregion
@@ -40,6 +49,44 @@ namespace ssrsmsbuildtasks.DeploymentManger.Proxy.R2
             {
                 return this.Url.Substring(0, this.Url.LastIndexOf("/"));
             }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Perform forms authentication with the reporting service server
+        /// </summary>
+        /// <param name="credentials">The credentials to be used</param>
+        private void PerformFormsAuthentication(NetworkCredential credentials)
+        {
+            // Register a cookie-container to store the authentication token
+            this.CookieContainer = new CookieContainer();
+
+            // Call the LogonUser method to get authenticated
+            this.LogonUser(credentials.UserName, credentials.Password, "");
+        }
+
+        /// <summary>
+        /// Fetch credentials stored in userinfo-section in an URL
+        /// </summary>
+        /// <param name="reportingService2010Url">The URL</param>
+        /// <returns>The stored credentials or null if none found</returns>
+        private NetworkCredential GetCredentialsInUrl(string reportingService2010Url)
+        {
+            var uri = new Uri(reportingService2010Url);
+            var userInfo = uri.UserInfo;
+            if (!String.IsNullOrEmpty(userInfo))
+            {
+                var idx = userInfo.IndexOf(":");
+                return new NetworkCredential
+                {
+                    UserName = userInfo.Substring(0, idx),
+                    Password = userInfo.Substring(idx + 1)
+                };
+            }
+            return null;
         }
 
         #endregion
